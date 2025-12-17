db3cc448494aaab768e8313c004996adddee8a6c
Siddhartha Singh <siddharthagithub0007@gmail.com>
2025-12-17 23:02:58 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -12,7 +12,7 @@ const AuthContext = createContext();
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
-    if (!context) {
+    if (!context) {                                                                                                           
         throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
